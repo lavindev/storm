c939de03cad231f075fb5a511971c3ac0a36ad3b
Alessandro Bellina <abellina@yahoo-inc.com>
2016-12-14 13:41:52 -0600

    SimpleGauge from Taylors metrics_v2

---
+++ storm-core/src/jvm/org/apache/storm/metrics2/SimpleGauge.java
@@ -0,0 +1,20 @@
+package org.apache.storm.metrics2;
+
+import com.codahale.metrics.Gauge;
+
+public class SimpleGauge<T> implements Gauge<T> {
+    private T value;
+
+    public SimpleGauge(T value){
+        this.value = value;
+    }
+
+    @Override
+    public T getValue() {
+        return this.value;
+    }
+
+    public void update(T value){
+        this.value = value;
+    }
+}
